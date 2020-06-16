a1cee42492614d1adefc0bdeec7432527fdb7f18
Chris Lima <chrislima@Chriss-iMac.local>
2020-06-16 18:29:55 -0400

    added middleware and set up database

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
 
+app.use(express.json());
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  Birds.find({}, (err, foundBirds) => {
+    res.json(foundBirds);
+  })
 })
+
+mongoose.connect('mongodb://localhost:27017/birds', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+    console.log('connected to mongod...');
+});
 
 
 app.listen(3000, () => {
